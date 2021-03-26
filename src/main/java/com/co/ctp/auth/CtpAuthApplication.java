e25915363a6f1275678c7ab6ed2591c4259e5149
Luis Eduardo Bolivar Cardenas <TheLuisBolivar@Juans-MacBook-Pro.local>
2021-03-25 19:18:56 -0500

    ready to deploy on dev and qa

--- src/main/java/com/co/ctp/auth/CtpAuthApplication.java
+++ src/main/java/com/co/ctp/auth/CtpAuthApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CtpAuthApplication {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CtpAuthApplication.class, args);
 	}
 }
