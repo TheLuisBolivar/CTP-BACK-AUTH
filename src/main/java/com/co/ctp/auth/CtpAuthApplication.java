3b8e9e68ad2221e6ed860bc1cd2384d319f931e1
TheLuisBolivar <lebolivarc@gmail.com>
2021-03-25 19:28:24 -0500

    Feature/mts 58 (#1) (#2)
    * Add first endpoint and login schema to start developing
    
    * ready to push
    
    * ready to deploy on dev and qa
    
    Co-authored-by: TheLuisADL <luis.bolivar@avaldigitallabs.com>
    Co-authored-by: Luis Eduardo Bolivar Cardenas <TheLuisBolivar@Juans-MacBook-Pro.local>
    
    Co-authored-by: TheLuisADL <luis.bolivar@avaldigitallabs.com>
    Co-authored-by: Luis Eduardo Bolivar Cardenas <TheLuisBolivar@Juans-MacBook-Pro.local>

---
+++ src/main/java/com/co/ctp/auth/CtpAuthApplication.java
@@ -0,0 +1,12 @@
+package com.co.ctp.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtpAuthApplication {
+	public static void main(String[] args) {
+		
+		SpringApplication.run(CtpAuthApplication.class, args);
+	}
+}
